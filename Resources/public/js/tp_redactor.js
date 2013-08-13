f731eb92e3ec8ed3e11d70856f7c9c4b7a6eeee0
Ben Scholler <ben.scholler@gmail.com>
2013-08-13 16:11:38 -0700

    Wrapping redactor in document.ready.
    This allows the tp_redactor.js file to be included anywhere on the page instead of after the text box redactor is to be applied to.

--- Resources/public/js/tp_redactor.js
+++ Resources/public/js/tp_redactor.js
@@ -1,30 +1,31 @@
 (function($) {
-    // If no redactor type used, just return
-    if (undefined === window.tp_redactor) {
-        return;
-    }
+    $(document).ready(function () {
+        // If no redactor type used, just return
+        if (undefined === window.tp_redactor) {
+            return;
+        }
 
-    /**
-     * Default redactor options.
-     * Values are sets at domready (for Routing dependency)
-     */
-    var defaultOptions = null;
+        /**
+         * Default redactor options.
+         * Values are sets at domready (for Routing dependency)
+         */
+        var defaultOptions = null;
 
-    /**
-     * Transform a form field into a redactor editor.
-     * Needs the id as context
-     */
-    var redactorize = function(id) {
-        $('#'+id).redactor($.extend({}, defaultOptions, this));
-    };
-
-    $(function(){
-        defaultOptions = {
-            imageGetJson: Routing.generate('_redactor_images'),
-            imageUpload:  Routing.generate('_redactor_upload'),
-            shortcuts:    false
+        /**
+         * Transform a form field into a redactor editor.
+         * Needs the id as context
+         */
+        var redactorize = function(id) {
+            $('#'+id).redactor($.extend({}, defaultOptions, this));
         };
-        $.each(window.tp_redactor, redactorize);
-    });
 
+        $(function(){
+            defaultOptions = {
+                imageGetJson: Routing.generate('_redactor_images'),
+                imageUpload:  Routing.generate('_redactor_upload'),
+                shortcuts:    false
+            };
+            $.each(window.tp_redactor, redactorize);
+        });
+    });
 }(jQuery));
